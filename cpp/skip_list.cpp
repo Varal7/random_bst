fd3e1e1a4f7d6d2233366939bcc1f3a24e878357
Chris McNally <mcnallychristopherm@gmail.com>
2018-12-04 13:05:50 -0500

    C++ skip lists

---
+++ cpp/skip_list.cpp
@@ -0,0 +1,130 @@
+#include "skip_list.h"
+
+
+// Node constructor
+node::node(int k, int v, int l) : key(k), value(v), level(l)
+// Allocate space for forward vector.
+{
+  forward.reserve(level);
+}
+
+
+// Skip list constructor
+skip_list::skip_list(int max_level, float prob)
+  : max_level_(max_level), prob_(prob)
+{
+  level_ = 0;           // No real nodes yet
+  header_ = new node(INT_MIN, 0, max_level_);
+  nil_ = new node(INT_MAX, 0, max_level_);
+  for( int i = 0; i < max_level_; i++) {
+    header_->forward[i] = nil_;
+    nil_->forward[i] = nullptr;
+  }
+}
+
+
+// Skip list destructor
+
+skip_list::~skip_list()
+{
+  // TODO: delete all the other nodes!
+  delete header_;
+  delete nil_;
+}
+
+
+int skip_list::random_level()
+{
+  int lvl = 1;
+  while ((float) rand()/(float) RAND_MAX < prob_)
+    lvl++;
+  return lvl;
+}
+
+
+void skip_list::insert(int search_key, int new_value)
+{
+  // Vector of rightmost nodes at each level to left of inserted node
+  std::vector<node*> update(max_level_);
+  // Maintain the current node
+  node* x = header_;
+  for(int i = level_ - 1; i >= 0; i--) {
+      while (x->forward[i] != nil_ && x->forward[i]->key < search_key) {
+        x = x->forward[i];
+      }
+      // Set the search path values
+      update[i] = x;
+  }
+  x = x->forward[0];
+
+  // Update value if key is already in the skip list.
+  if (x->key == search_key) {
+    x->value = new_value;
+  }
+
+  //Otherwise, insert the new key-value pair.
+  else {
+    int new_level = random_level();
+    // Update the list's level if the new new node is higher-level
+    if (new_level > level_) {
+      for (int i = level_; i < new_level; i++) {
+        update[i] = header_;
+      }
+      level_ = new_level;
+    }
+    // Allocate a new node for the inserted key-value pair and set key, value.
+    node* x = new node(search_key, new_value, new_level);
+    // Stitch the new node into the list by moving local pointers.
+    for (int i = 0; i < new_level; i++) {
+      x->forward[i] = update[i]->forward[i];
+      update[i]->forward[i] = x;
+    }
+  }
+}
+
+void skip_list::del(int search_key)
+{
+  // Vector of rightmost nodes at each level to left of inserted node
+  std::vector<node*> update(max_level_);
+  // Maintain the current node
+  node* x = header_;
+  for(int i = level_ - 1; i >= 0; i--) {
+      while (x->forward[i] != nil_ && x->forward[i]->key < search_key) {
+        x = x->forward[i];
+      }
+      // Set the search path values
+      update[i] = x;
+  }
+  x = x->forward[0];
+  // Now remove the key-value pair
+  if (x->key == search_key) {
+    for (int i = 0; i < level_; i++) {
+      if (update[i]->forward[i] != x) break;
+      update[i]->forward[i] = x->forward[i];
+    }
+    delete x;
+  }
+  while (level_ > 1 && header_->forward[level_] == nil_) {
+    level_ = level_-1;
+  }
+}
+
+
+node* skip_list::search(int search_key)
+{
+  // Start looking at the data structure's entry point
+  node* x = header_;
+  // Loop invariant: x-> < search_key
+  for (int i = level_ - 1; i >= 0; i--) {
+    while ((x->forward[i] != nil_) && (x->forward[i]->key < search_key)) {
+      x = x->forward[i];
+    }
+  }
+  // x->key < search_key <= x->forward[0]->key
+  x = x->forward[0];
+  if (x->key == search_key) {
+    return x;
+  } else {
+    return nullptr;
+  }
+}
---
+++ cpp/skip_list.h
@@ -0,0 +1,39 @@
+#include <vector>
+
+#ifndef SKIP_LIST_H_
+#define SKIP_LIST_H_
+
+// A simple struct for nodes of a skip list.
+struct node
+{
+  int key;
+  // Can we safely make assumptions such as 'values are all nonnegative'?
+  int value;
+  // Number of forward pointers
+  int level;
+  // Following nodes
+  // Are dynamic arrays admissible in this sort of application?
+  std::vector<node*> forward;
+  node(int k, const int v, int level);
+};
+
+// The Skip List class proper
+class skip_list
+{
+public:
+  skip_list(int max_level, float prob);
+  ~skip_list();
+  // methods of the data structure
+  void insert(int search_key, int new_value);
+  void del(int search_key);
+  node* search(int search_key);
+//private:
+  int max_level_;     // Maximum admissible level
+  int level_;         // Actual level: highest level of any node
+  float prob_;        // Node level ratio
+  node* header_;
+  node* nil_;
+  int random_level(); // Calculates level of a node.
+};
+
+#endif  // SKIP_LIST_H_
