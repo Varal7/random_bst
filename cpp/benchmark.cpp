96bf9e09d5b77a296e771b6f03c147713d5b0c8e
Victor Quach <varaal@gmail.com>
2018-12-05 14:45:00 -0500

    Add templated test

--- cpp/benchmark.cpp
+++ cpp/benchmark.cpp
@@ -10,20 +10,29 @@
 using namespace std;
 
 
-static void BM_ZipTreeInsertRand(benchmark::State& state) {
-  ZipTree z;
+template <class D, D*& d> void BM_DictionaryInsertRand(benchmark::State& state) {
   for (auto _ : state) {
     state.PauseTiming();
     for (int j = 0; j < state.range(0); ++j) {
-      z.insert(rand(), 0);
+      d->insert(rand(), 0);
     }
     state.ResumeTiming();
     for (int j = 0; j < state.range(1); ++j) {
-      z.insert(rand(), 0);
+      d->insert(rand(), 0);
     }
+    state.SetItemsProcessed(state.iterations() * state.range(1));
   }
 }
 
-BENCHMARK(BM_ZipTreeInsertRand)->Ranges({{1<<10, 8<<10}, {128, 512}});
+//BENCHMARK(BM_DictionaryInsertRand, ZipTree)->RangeMultiplier(2)->Ranges({{1<<8, 8<<8}, {128, 512}});
+//
+ZipTree* z = new ZipTree;
+BENCHMARK_TEMPLATE(BM_DictionaryInsertRand, ZipTree, z)->RangeMultiplier(2)->Ranges({{1<<8, 1<<8}, {128, 128}});
+
+Treap* t = new Treap;
+BENCHMARK_TEMPLATE(BM_DictionaryInsertRand, Treap, t)->RangeMultiplier(2)->Ranges({{1<<8, 1<<8}, {128, 128}});
+
+SkipList* s = new SkipList(16, 0.5);
+BENCHMARK_TEMPLATE(BM_DictionaryInsertRand, SkipList, s)->RangeMultiplier(2)->Ranges({{1<<8, 1<<8}, {128, 128}});
 
 BENCHMARK_MAIN();
